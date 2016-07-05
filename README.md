d17020241d36c99660d8f0cae74e202ff13825c2
dfinke <finked@hotmail.com>
2016-07-05 09:13:02 -0400

    Added

---
+++ README.md
@@ -0,0 +1,12 @@
+# Fuzzy Search
+
+```
+.\countries.ps1 | Select-FuzzyString pia
+.\countries.ps1 | Select-FuzzyString uae
+.\countries.ps1 | Select-FuzzyString svg
+.\countries.ps1 | Select-FuzzyString bh
+
+.\countries.ps1 | sfs united
+```
+
+## In Action
---
+++ Select-FuzzyString.ps1
@@ -0,0 +1,12 @@
+﻿function Select-FuzzyString {
+    param(        
+        $search='',
+        [parameter(ValueFromPipeline=$true)]
+        $data
+    )
+    
+    Begin   { $search = $search.ToCharArray() -join '.*?' } 
+    Process { if($data -match $search) { $data } }
+}
+
+set-alias sfs select-fuzzystring
