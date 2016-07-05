a77c3425f09fbfd545c0e9c63468eda708f84ba9
dfinke <finked@hotmail.com>
2016-07-05 09:34:29 -0400

    added

---
+++ TryFuzzyString.ps1
@@ -0,0 +1,16 @@
+. .\Select-FuzzyString.ps1
+
+'Select-FuzzyString pia'
+.\countries.ps1 | Select-FuzzyString pia
+''
+'Select-FuzzyString uae'
+.\countries.ps1 | Select-FuzzyString uae
+''
+'Select-FuzzyString svg'
+.\countries.ps1 | Select-FuzzyString svg
+''
+'Select-FuzzyString bh'
+.\countries.ps1 | Select-FuzzyString bh
+''
+'sfs united'
+.\countries.ps1 | sfs united
