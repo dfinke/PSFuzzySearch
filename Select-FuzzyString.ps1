0521309d6caea1b022321ce9ba3f3e0658cb8c95
Chris Hunt <cdhunt@gmail.com>
2016-07-06 13:16:44 -0400

    Adds Get-FuzzyCommand and Get-FuzzyChildItem

--- Select-FuzzyString.ps1
+++ Select-FuzzyString.ps1
@@ -9,4 +9,33 @@
     Process { if($data -match $search) { $data } }
 }
 
+function Get-FuzzyCommand {
+    param(
+        # Search String
+        [Parameter(Mandatory)]
+        [string]
+        $search = ''
+    )
+
+    Get-Command | Select-FuzzyString -Search $search
+}
+
+function Get-FuzzyChildItem {
+        param(
+        # Search String
+        [Parameter(Mandatory, Position=0)]
+        [string]
+        $search = '',
+        # Specifies a path to one or more locations.
+        [Parameter( Position=1,
+                   HelpMessage="Path to one or more locations.")]
+        [Alias("PSPath")]
+        [ValidateNotNullOrEmpty()]
+        [string[]]
+        $Path = $PWD
+    )
+
+    Get-ChildItem -Path $Path | Select-FuzzyString -Search $search
+}
+
 set-alias sfs select-fuzzystring
