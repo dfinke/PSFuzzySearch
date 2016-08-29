e8d0c84fc2b122e9ece794d08c9ac132280dd504
Chris Hunt <cdhunt@gmail.com>
2016-08-29 09:11:43 -0400

    Adds PSReadline Handler for Select-FuzzyCommand
    Binds to Ctrl+P

---
+++ PSReadLine_FuzzyCommand.ps1
@@ -0,0 +1,103 @@
+function Find-CurrentCommand {
+	param([string]$line,[int]$cursor,[ref]$leftCursor,[ref]$rightCursor)
+	
+	if ($line.Length -eq 0) {
+		$leftCursor.Value = $rightCursor.Value = 0
+		return $null
+	}
+
+	if ($cursor -ge $line.Length) {
+		$leftCursorTmp = $cursor - 1
+	} else {
+		$leftCursorTmp = $cursor
+	}
+	:leftSearch for (;$leftCursorTmp -ge 0;$leftCursorTmp--) {
+		if ([string]::IsNullOrWhiteSpace($line[$leftCursorTmp])) {
+			if (($leftCursorTmp -lt $cursor) -and ($leftCursorTmp -lt $line.Length-1)) {
+				$leftCursorTmpQuote = $leftCursorTmp - 1
+				$leftCursorTmp = $leftCursorTmp + 1
+			} else {
+				$leftCursorTmpQuote = $leftCursorTmp
+			}
+			for (;$leftCursorTmpQuote -ge 0;$leftCursorTmpQuote--) {
+				if (($line[$leftCursorTmpQuote] -eq '"') -and (($leftCursorTmpQuote -le 0) -or ($line[$leftCursorTmpQuote-1] -ne '"'))) {
+					$leftCursorTmp = $leftCursorTmpQuote
+					break leftSearch
+				}
+				elseif (($line[$leftCursorTmpQuote] -eq "'") -and (($leftCursorTmpQuote -le 0) -or ($line[$leftCursorTmpQuote-1] -ne "'"))) {
+					$leftCursorTmp = $leftCursorTmpQuote
+					break leftSearch
+				}
+			}
+			break leftSearch
+		}
+	}
+	:rightSearch for ($rightCursorTmp = $cursor;$rightCursorTmp -lt $line.Length;$rightCursorTmp++) {
+		if ([string]::IsNullOrWhiteSpace($line[$rightCursorTmp])) {
+			if ($rightCursorTmp -gt $cursor) {
+				$rightCursorTmp = $rightCursorTmp - 1
+			}
+			for ($rightCursorTmpQuote = $rightCursorTmp+1;$rightCursorTmpQuote -lt $line.Length;$rightCursorTmpQuote++) {
+				if (($line[$rightCursorTmpQuote] -eq '"') -and (($rightCursorTmpQuote -gt $line.Length) -or ($line[$rightCursorTmpQuote+1] -ne '"'))) {
+					$rightCursorTmp = $rightCursorTmpQuote
+					break rightSearch
+				}
+				elseif (($line[$rightCursorTmpQuote] -eq "'") -and (($rightCursorTmpQuote -gt $line.Length) -or ($line[$rightCursorTmpQuote+1] -ne "'"))) {
+					$rightCursorTmp = $rightCursorTmpQuote
+					break rightSearch
+				}
+			}
+			break rightSearch
+		}
+	}
+	if ($leftCursorTmp -lt 0 -or $leftCursorTmp -gt $line.Length-1) { $leftCursorTmp = 0}
+	if ($rightCursorTmp -ge $line.Length) { $rightCursorTmp = $line.Length-1 }
+	$leftCursor.Value = $leftCursorTmp
+	$rightCursor.Value = $rightCursorTmp
+	$str = -join ($line[$leftCursorTmp..$rightCursorTmp])
+	return $str.Trim("'").Trim('"')
+}
+
+function Invoke-FuzzyCommandPsReadlineHandler {
+	$leftCursor = $null
+	$rightCursor = $null
+	$line = $null
+	$cursor = $null
+	[Microsoft.PowerShell.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
+	$currentCommand = Find-CurrentCommand $line $cursor ([ref]$leftCursor) ([ref]$rightCursor)
+
+
+	$commands = Select-FuzzyCommand $currentCommand
+	if ($commands -ne $null) {
+
+		$command = $commands | Out-GridView -Title Commands -PassThru
+
+		if ($command) {
+            [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+            [Microsoft.PowerShell.PSConsoleReadLine]::Insert($command)
+        }	
+	}
+}
+
+# install PSReadline shortcut:
+if (Get-Module -ListAvailable -Name PSReadline) {
+
+	if ($args.Length -ge 1) {
+		$script:PSReadlineHandlerChord = $args[0] 
+	} 
+    else {
+		$script:PSReadlineHandlerChord = 'Ctrl+P'
+	}
+
+	if (Get-PSReadlineKeyHandler -Bound | Where Key -eq $script:PSReadlineHandlerChord) {
+		Write-Warning ("PSReadline chord {0} already in use - keyboard handler not installed" -f $script:PSReadlineHandlerChord)
+	} 
+    else {
+		Set-PSReadlineKeyHandler -Key Ctrl+P -BriefDescription "SelectFuzzyCommand" -ScriptBlock  {
+			Invoke-FuzzyCommandPsReadlineHandler
+		}
+	} 
+} 
+else {
+	Write-Warning "PSReadline module not found - keyboard handler not installed" 
+}
