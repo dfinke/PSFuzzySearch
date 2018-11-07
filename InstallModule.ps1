5cd1af79f46f9bbedd4557ae82b9cb52fa0cc499
dfinke <finked@hotmail.com>
2018-11-07 09:47:29 -0500

    Updated install module

--- InstallModule.ps1
+++ InstallModule.ps1
@@ -1,3 +1,3 @@
 $fullPath = 'C:\Program Files\WindowsPowerShell\Modules\PSFuzzySearch'
 
-Robocopy . $fullPath /mir /XD .vscode .git media examples /XF appveyor.yml .gitattributes .gitignore
+Robocopy . $fullPath /mir /XD .vscode .git media /XF appveyor.yml .gitattributes .gitignore
